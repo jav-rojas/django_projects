c3b32fc7315636dea703d556fa3566df689e983b
jrojasc <54907635+jav-rojas@users.noreply.github.com>
2020-09-30 14:25:47 -0300

    Actualización de archivos desde DJ4E-Samples
    Se actualizan los archivos settings.py y urls.py a partir de los archivos del mismo nombre en DJ4E-Samples.
    
    Se actualiza .gitignore para dejar fuera el venv creado especialmente.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,27 +15,56 @@ Including another URLconf
 """
 import os
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf import settings
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
-from django.views.generic import TemplateView
-
-
-# Subimos dos carpetas desde donde se ejecuta el archivo
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# SITE_ROOT corresponde a la ruta de la carpeta 'site'
-SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    # De archivo en dj4e-samples
+    path('', include('home.urls')),  # Change to ads.urls
+    path('admin/', admin.site.urls),  # Keep
+    path('accounts/', include('django.contrib.auth.urls')),  # Keep
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
+
+    # Aplicaciones propias
     path('cats/', include('cats.urls')),
     path('autos/', include('autos.urls')),
     path('polls/', include('polls.urls')),
     path('hello/', include('hello.urls')),
-    path('admin/', admin.site.urls),
-    url(r'site/(?P<path>.*)$', serve,
-        {'document_root': SITE_ROOT, 'show_indexes': True},
-        name='site_path'
-    ),
-    path('', TemplateView.as_view(template_name='home/main.html')),
 ]
+
+# Serve the static HTML
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    url(r'^site/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
+        name='site_path'
+        ),
+]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
+]
+
+# Switch to social login if it is configured - Keep for later
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0,
+                       path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+                       )
+    print('Using', social_login, 'as the login template')
+except:
+    print('Using registration/login.html as the login template')
+
+# References
+
+# https://docs.djangoproject.com/en/3.0/ref/urls/#include
