97fc098f24869b9b43f0b29d909ba6b0448e018a
jav-rojas <54907635+jav-rojas@users.noreply.github.com>
2020-12-31 18:10:10 -0300

    Agrega cambios necesarios para completar assignment

--- mysite/ads/urls.py
+++ mysite/ads/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, reverse_lazy
 from . import views
+from django.views.generic import TemplateView
 
 app_name='ads'
 urlpatterns = [
@@ -11,6 +12,5 @@ urlpatterns = [
         views.AdUpdateView.as_view(success_url=reverse_lazy('ads:all')), name='ad_update'),
     path('ad/<int:pk>/delete', 
         views.AdDeleteView.as_view(success_url=reverse_lazy('ads:all')), name='ad_delete'),
+    path('ad_picture/<int:pk>', views.stream_file, name='ad_picture'),
 ]
-
-# We use reverse_lazy in urls.py to delay looking up the view until all the paths are defined
